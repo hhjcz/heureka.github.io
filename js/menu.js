d8029089ccd54ffd1c90b5a686baf40c7c888e4c
Peta Sittek <peta.sittek@gmail.com>
2016-06-03 18:05:17 +0200

    Add easter eggs

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,14 @@
 'use strict';
 
+/**
+ * Cool you opened this!
+ *
+ * Seems you're quite curious! And we like that!
+ * This code is intentionally left unminified so you can read our special offer...
+ * If you find anything nasty here - what about having a beer over it? We pay! ;)
+ *
+ */
+
 (function () {
     /**
      * Config
